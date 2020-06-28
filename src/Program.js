f49233800c542b0bdc23effbc2c8b6b4b898ceee
qinshou2017 <czy-ken@163.com>
2020-06-28 12:01:53 -0700

    refactor Render

---
+++ src/Program.js
@@ -0,0 +1,120 @@
+
+function createShader(ctx, type, src) {
+    const s = ctx.createShader(type);
+    ctx.shaderSource(s, src);
+    ctx.compileShader(s);
+    if (!ctx.getShaderParameter(s, ctx.COMPILE_STATUS))
+        throw "error compile " + (type === ctx.VERTEX_SHADER? "vertex": "fragment") + " shader: "
+            + ctx.getShaderInfoLog(s);
+    return s;
+}
+
+function createProgram(ctx, vertShader, fragShader) {
+    const p = ctx.createProgram();
+    ctx.attachShader(p, vertShader);
+    ctx.attachShader(p, fragShader);
+    ctx.linkProgram(p);
+    if (!ctx.getProgramParameter(p, ctx.LINK_STATUS))
+        throw "error link program: " + ctx.getProgramInfoLog(p);
+    return p;
+}
+
+class Program {
+    constructor(ctx, vertSrc, fragSrc) {
+        this.ctx = this.gl = ctx;
+        let prog = this.prog = this.program = createProgram(ctx,
+            createShader(ctx, ctx.VERTEX_SHADER, vertSrc),
+            createShader(ctx, ctx.FRAGMENT_SHADER, fragSrc)
+        );
+        const getCurrentVars = (varsType, aou = varsType === ctx.ACTIVE_ATTRIBUTES? "Attrib": "Uniform") =>
+            [...Array(ctx.getProgramParameter(prog, varsType))]
+            .map((_, i) => {
+                const {size, type, name} = ctx["getActive" + aou](prog, i),
+                      loc                = ctx[`get${aou}Location`](prog, name);
+                return {size, type, name: name.split("[")[0], loc};
+            })
+            .reduce((ac, {name, size, type, loc}) => {
+                ac[name] = {name, size, type, loc};
+                return ac;
+            }, {});
+        this.vars = {
+            atts: getCurrentVars(ctx.ACTIVE_ATTRIBUTES),
+            unis: getCurrentVars(ctx.ACTIVE_UNIFORMS)
+        };
+    };
+    use() { this.ctx.useProgram(this.prog); return this; };
+    getAtt(name) { return this.vars.atts[name].loc; };
+    getUni(name) { return this.vars.unis[name].loc; };
+    setAtt(name, bufferData, size = undefined, attDataType = this.ctx.FLOAT, normalized = false, stride = 0, offset = 0) {
+        const ctx = this.ctx, att = this.vars.atts[name];
+        if (!att) throw "Cannot get attribute " + name;
+        if (size === undefined) {
+            switch (att.type) {
+                case ctx.FLOAT:
+                    size = 1; break;
+                case ctx.FLOAT_VEC2: case ctx.FLOAT_MAT2:
+                    size = 2; break;
+                case ctx.FLOAT_VEC3: case ctx.FLOAT_MAT3:
+                    size = 3; break;
+                case ctx.FLOAT_VEC4: case ctx.FLOAT_MAT4:
+                    size = 4; break;
+                default:
+                    console.error("Don't know gl type", att.type, "for attribute", att.name);
+                    throw "Don't know attribute type";
+            }
+        }
+        let bufferType = bufferData.type || ctx.ARRAY_BUFFER;
+        ctx.bindBuffer(bufferType, bufferData);
+        ctx.enableVertexAttribArray(att.loc);
+        ctx.vertexAttribPointer(att.loc, size, attDataType, normalized, stride, offset);
+        ctx.bindBuffer(bufferType, null);
+        return this;
+    };
+    setUni(name, value) {
+        const ctx = this.ctx, uni = this.vars.unis[name];
+        switch (uni.type) {
+            case ctx.FLOAT_MAT4:
+                ctx.uniformMatrix4fv(uni.loc, false, value);
+                break;
+            case ctx.FLOAT_MAT3:
+                ctx.uniformMatrix3fv(uni.loc, false, value);
+                break;
+            case ctx.FLOAT_MAT2:
+                ctx.uniformMatrix2fv(uni.loc, false, value);
+                break;
+            case ctx.FLOAT:
+                ctx.uniform1f(uni.loc, value);
+                break;
+            case ctx.INT: case ctx.SAMPLER_CUBE: case ctx.SAMPLER_2D:
+                ctx.uniform1i(uni.loc, value);
+                break;
+            case ctx.FLOAT_VEC2:
+                ctx.uniform2fv(uni.loc, value);
+                break;
+            case ctx.FLOAT_VEC3:
+                ctx.uniform3fv(uni.loc, value);
+                break;
+            case ctx.FLOAT_VEC4:
+                ctx.uniform4fv(uni.loc, value);
+                break;
+            case ctx.INT_VEC2:
+                ctx.uniform2iv(uni.loc, value);
+                break;
+            case ctx.INT_VEC3:
+                ctx.uniform3iv(uni.loc, value);
+                break;
+            case ctx.INT_VEC4:
+                ctx.uniform4iv(uni.loc, value);
+                break;
+            default:
+                console.warn("Don't know gl type", uni.type, "for uniform", uni.name);
+                throw "Don't know uniform type";
+        }
+        return this;
+    };
+};
+
+export {
+    Program,
+    Program as default
+};
---
+++ src/Render.js
@@ -0,0 +1,94 @@
+import Program from "./Program.js";
+
+class Render {
+    constructor(canvas) {
+        let ctx = this.gl = this.ctx =
+            canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+        if (!ctx) throw "Cannot get the WebGL context";
+        this.prgCache = {};
+        this.texCache = {};
+        this.frame = this.frame.bind(this);
+        this.timer = null;
+    };
+    get aspectRatio() {
+        return this.ctx.canvas.width / this.ctx.canvas.height;
+    };
+    createProgram(name, vectSrc, fragSrc) {
+        return this.prgCache[name] = new Program(this.ctx, vectSrc, fragSrc);
+    };
+    getProgram(name) { return this.prgCache[name]; };
+
+    frame(timestamp) {
+        if (this.onRender) this.onRender(timestamp);
+        this.timer = window.requestAnimationFrame(this.frame);
+    };
+    play() {
+        if (this.timer === null) this.frame();
+    };
+    stop() {
+        window.cancelAnimationFrame(this.timer);
+        this.timer = null;
+    };
+
+    setSize(w, h) {
+        const c = this.ctx.canvas;
+        c.width = w; c.height = h;
+        this.ctx.viewport(0, 0, w, h);
+        return {w, h};
+    };
+
+    fitScreen(wp = 1, hp = 1) {
+        return this.setSize(
+            window.innerWidth * wp,
+            window.innerHeight * hp
+        );
+    };
+
+    createIbo(data, drawType = this.ctx.STATIC_DRAW) {
+        if (!(data.buffer instanceof ArrayBuffer))
+            data = new Int16Array(data);
+        const {ctx} = this,
+              ibo  = ctx.createBuffer();
+        ctx.bindBuffer(ctx.ELEMENT_ARRAY_BUFFER, ibo);
+        ctx.bufferData(ctx.ELEMENT_ARRAY_BUFFER, data, drawType);
+        ctx.bindBuffer(ctx.ELEMENT_ARRAY_BUFFER, null);
+        ibo.type = ctx.ELEMENT_ARRAY_BUFFER;
+        ibo.length = data.length;
+        return ibo;
+    };
+
+    createVbo(data, drawType = this.ctx.STATIC_DRAW) {
+        if (!(data.buffer instanceof ArrayBuffer))
+            data = new Float32Array(data);
+        const {ctx} = this,
+              vbo  = ctx.createBuffer();
+        ctx.bindBuffer(ctx.ARRAY_BUFFER, vbo);
+        ctx.bufferData(ctx.ARRAY_BUFFER, data, drawType);
+        ctx.bindBuffer(ctx.ARRAY_BUFFER, null);
+        vbo.type = ctx.ARRAY_BUFFER;
+        return vbo;
+    };
+
+    createTexture(img, name = img.outerHTML.match(/src="([^"]*)"/)[1], doYFlip = false) {
+        const {ctx} = this,
+              tex  = ctx.createTexture();
+        if (doYFlip) ctx.pixelStorei(ctx.UNPACK_FLIP_Y_WEBGL, true);
+        ctx.bindTexture(ctx.TEXTURE_2D, tex);
+        ctx.texParameteri(ctx.TEXTURE_2D, ctx.TEXTURE_MAG_FILTER, ctx.NEAREST);
+        ctx.texParameteri(ctx.TEXTURE_2D, ctx.TEXTURE_MIN_FILTER, ctx.NEAREST);
+        ctx.texParameteri(ctx.TEXTURE_2D, ctx.TEXTURE_WRAP_S, ctx.CLAMP_TO_EDGE);
+        ctx.texParameteri(ctx.TEXTURE_2D, ctx.TEXTURE_WRAP_T, ctx.CLAMP_TO_EDGE);
+        ctx.texImage2D(ctx.TEXTURE_2D, 0, ctx.RGBA, ctx.RGBA, ctx.UNSIGNED_BYTE, img);
+        ctx.generateMipmap(ctx.TEXTURE_2D);
+        ctx.bindTexture(ctx.TEXTURE_2D, null);
+        if (doYFlip) ctx.pixelStorei(ctx.UNPACK_FLIP_Y_WEBGL, false);
+        this.texCache[name] = tex;
+        return tex;
+    };
+    getTexture(name) { return this.texCache[name]; };
+};
+
+export {
+    Render,
+    Render as default
+};
