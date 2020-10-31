dd387a5da400b5c7b6d14b89a8415cbe3252eaed
qinshou2017 <czy-ken@163.com>
2020-10-31 06:41:31 -0700

    add full-screen button in playing game interface

--- src/playGame.js
+++ src/playGame.js
@@ -3,8 +3,28 @@ import spa from "./spa.js";
 let worldRender = null, world = null;
 
 spa.addEventListener("play_game_page", "load", (pageID, data) => {
+    let requestFullscreen = document.body.requestFullscreen ||  document.body.mozRequestFullScreen ||  document.body.webkitRequestFullScreen ||  document.body.msRequestFullscreen;
+    requestFullscreen = requestFullscreen.bind(document.body);
+    const isFullscreen = () => document.body === (document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement);
     if (!window.isTouchDevice)
         document.getElementsByClassName("mc-move-buttons")[0].style.display = "none";
+    else {
+        let fullBtn = document.getElementsByClassName("full-screen-btn")[0];
+        if (!isFullscreen()) fullBtn.style.display = "";
+        document.body.onfullscreenchange =
+        document.body.onmozfullscreenchange =
+        document.body.onwebkitfullscreenchange =
+        document.body.MSFullscreenChange = function(e) {
+            if (e.target === null) return;
+            console.log("asdf", isFullscreen())
+            if (!isFullscreen()) {
+                fullBtn.style.display = "";
+                spa.openPage("stop_game_page");
+            }
+            else fullBtn.style.display = "none";
+        };
+        fullBtn.onclick = requestFullscreen;
+    }
     if (worldRender === null) spa.openPage("loading_terrain_page");
 });
 spa.addEventListener("play_game_page", "unload", (pageID, data) => {
