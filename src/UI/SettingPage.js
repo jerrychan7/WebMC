428b735328436702a92399270f690f92bb8b6c86
qinshou2017 <czy-ken@163.com>
2022-06-20 14:48:45 -0700

    update setting: change homepage blur

--- src/UI/SettingPage.js
+++ src/UI/SettingPage.js
@@ -1,6 +1,8 @@
 
-import { Page } from "./Page.js";
+import { Page, pm } from "./Page.js";
 import { World } from "../World/World.js";
+
+// TODO: 使用单个设置单例来同步设置以及持久化
 
 class SettingPage extends Page {
     static get shortPageID() { return "setting"; };
@@ -18,6 +20,15 @@ class SettingPage extends Page {
                 World.config.terrain = btn.innerHTML;
             };
         }
+        this.homepageBlur = this.shadowRoot.getElementById("homepage-blur");
+        let blurLevel = (window.getComputedStyle(
+            pm.getPageByID("welcome").bgCanvas).filter.replace(/[^\d.]+/g, "") * window.devicePixelRatio).toFixed(1);
+        this.homepageBlur.setAttribute("value", blurLevel);
+        this.homepageBlur.setAttribute("progress", blurLevel);
+        this.homepageBlur.addEventListener("valueChange", ({ detail }) => {
+            pm.getPageByID("welcome").bgCanvas.style.filter = `blur(calc(${detail}px / var(--device-pixel-ratio)))`;
+            this.homepageBlur.setAttribute("progress", (+detail).toFixed(1));
+        });
     };
     onHistoryBack() { this.close(); };
 };
