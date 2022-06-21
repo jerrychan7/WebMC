6f2aa134284ebaf435d149b78c9ff67e9bb864e7
qinshou2017 <czy-ken@163.com>
2022-06-21 08:52:17 -0700

    fix setting page cannot be opened when playing game

--- src/UI/SettingPage.js
+++ src/UI/SettingPage.js
@@ -20,24 +20,26 @@ class SettingPage extends Page {
                 World.config.terrain = btn.innerHTML;
             };
         }
-        this.homepageBlur = this.shadowRoot.getElementById("homepage-blur");
-        let blurLevel = (window.getComputedStyle(
-            pm.getPageByID("welcome").bgCanvas).filter.replace(/[^\d.]+/g, "") * window.devicePixelRatio).toFixed(1);
-        this.homepageBlur.setAttribute("value", blurLevel);
-        this.homepageBlur.setAttribute("progress", blurLevel);
-        this.homepageBlur.addEventListener("valueChange", ({ detail }) => {
-            pm.getPageByID("welcome").bgCanvas.style.filter = `blur(calc(${detail}px / var(--device-pixel-ratio)))`;
-            this.homepageBlur.setAttribute("progress", (+detail).toFixed(1));
-        });
-        this.homepageBlur.addEventListener("pointerdown", e => {
-            const x = this.homepageBlur.offsetLeft, y = this.homepageBlur.offsetTop;
-            const w = this.homepageBlur.offsetWidth, h = this.homepageBlur.offsetHeight;
-            this.style.clipPath = `polygon(${x}px ${y}px, ${x + w}px ${y}px, ${x + w}px ${y + h}px, ${x}px ${y + h}px)`;
-        });
-        for (let e of "pointerup,pointercancel,pointerout".split(","))
-            this.addEventListener(e, () => {
-                this.style.clipPath = null;
+        if (pm.getPageByID("welcome")) {
+            this.homepageBlur = this.shadowRoot.getElementById("homepage-blur");
+            let blurLevel = (window.getComputedStyle(
+                pm.getPageByID("welcome").bgCanvas).filter.replace(/[^\d.]+/g, "") * window.devicePixelRatio).toFixed(1);
+            this.homepageBlur.setAttribute("value", blurLevel);
+            this.homepageBlur.setAttribute("progress", blurLevel);
+            this.homepageBlur.addEventListener("valueChange", ({ detail }) => {
+                pm.getPageByID("welcome").bgCanvas.style.filter = `blur(calc(${detail}px / var(--device-pixel-ratio)))`;
+                this.homepageBlur.setAttribute("progress", (+detail).toFixed(1));
             });
+            this.homepageBlur.addEventListener("pointerdown", e => {
+                const x = this.homepageBlur.offsetLeft, y = this.homepageBlur.offsetTop;
+                const w = this.homepageBlur.offsetWidth, h = this.homepageBlur.offsetHeight;
+                this.style.clipPath = `polygon(${x}px ${y}px, ${x + w}px ${y}px, ${x + w}px ${y + h}px, ${x}px ${y + h}px)`;
+            });
+            for (let e of "pointerup,pointercancel,pointerout".split(","))
+                this.addEventListener(e, () => {
+                    this.style.clipPath = null;
+                });
+        }
     };
     onHistoryBack() { this.close(); };
 };
